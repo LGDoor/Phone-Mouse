76994b0aeea786467612732d8c35058f6303df2b
Raphael Yu <yyddxxmm@gmail.com>
2011-12-11 21:21:44 +0800

    注释

--- src/com/raphaelyu/phonemouse/ConnectActivity.java
+++ src/com/raphaelyu/phonemouse/ConnectActivity.java
@@ -7,9 +7,13 @@ public class ConnectActivity extends Activity {
 }
 
 // TODO: 协议 定长 type:1
-//            DISCOVERY:
-//            LOGIN:
-//            LOGOUT:
-//            HEARTBEAT:
-//            MOVE: timestamp:4/8 pos_x:4 pos_y:4
+
+/*
+ * 协议
+ * 
+ * DISCOVERY: "PHONEMOUSE"
+ * ECHO: "HERE "
+ * HEARTBEAT: X
+ * MOVE: timestamp:4/8 pos_x:4 pos_y:4
+ */
 // TODO: Service
