b2637557992c23da5b76c6db0f7a256b135147db
Raphael Yu <yyddxxmm@gmail.com>
2012-01-04 00:12:59 +0800

    Initial full contribution.

---
+++ PhoneMouse/src/com/raphaelyu/phonemouse/PhoneMouseActivity.java
@@ -0,0 +1,256 @@
+package com.raphaelyu.phonemouse;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.SocketAddress;
+import java.net.UnknownHostException;
+import java.nio.ByteBuffer;
+import java.nio.channels.DatagramChannel;
+
+import com.raphaelyu.R;
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.app.Dialog;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.ToggleButton;
+
+public class PhoneMouseActivity extends Activity implements SensorEventListener {
+    final static boolean TEST_MODE = false;
+    final static String TEST_SERVER_IP = "192.168.1.5";
+    final static short PHONE_MOUSE_PORT = 5329;
+    final static int MAX_PACKET_LENGTH = 32;
+    final static byte PACKET_TYPE_DISCOVER = 0x1;
+    final static byte PACKET_TYPE_REPLY = 0x2;
+    final static byte PACKET_TYPE_MOVE = 0x10;
+    private static final float EPSILON = 0.00f;
+
+    private float mLastX = 0.0f;
+
+    private float mLastY = 0.0f;
+
+    private TextView mTvAcceleration;
+
+    private ToggleButton mTbSwitch;
+
+    private CoordinateView mVwCoordinate;
+
+    private SensorManager mSensorManager;
+
+    private SocketAddress mServerAddr = null;
+
+    private ViewGroup mLayoutNoServer;
+
+    private ViewGroup mLayoutControl;
+
+    private Button mBtnRetry;
+
+    private class DiscoverTask extends AsyncTask<Void, Void, Void> {
+
+        Dialog mDialog;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            mDialog = ProgressDialog
+                    .show(PhoneMouseActivity.this, null, "searching...", true, true);
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            super.onPostExecute(result);
+            if (mServerAddr != null) {
+                mLayoutControl.setVisibility(View.VISIBLE);
+                mLayoutNoServer.setVisibility(View.GONE);
+            } else {
+                // TODO
+            }
+            mDialog.dismiss();
+        }
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            // Test mode
+            // - specified server address
+
+            if (TEST_MODE) {
+                mServerAddr = new InetSocketAddress(TEST_SERVER_IP, PHONE_MOUSE_PORT);
+                return null;
+            }
+
+            byte[] buf = new byte[1];
+            InetAddress broadcastIP;
+            try {
+                broadcastIP = Inet4Address.getByName("255.255.255.255");
+                try {
+                    DatagramSocket socket = new DatagramSocket();
+                    socket.setSoTimeout(8000);
+
+                    DatagramPacket packet = new DatagramPacket(buf, buf.length, broadcastIP,
+                            PHONE_MOUSE_PORT);
+                    buf[0] = PACKET_TYPE_DISCOVER;
+                    socket.send(packet);
+                    socket.receive(packet);
+                    if (packet.getLength() == 1) {
+                        byte[] buffer = packet.getData();
+                        if (buffer[0] != PACKET_TYPE_REPLY) {
+                            mServerAddr = null;
+                            return null;
+                        }
+                    } else {
+                        mServerAddr = null;
+                        return null;
+                    }
+                    mServerAddr = packet.getSocketAddress();
+                    socket.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            } catch (UnknownHostException e1) {
+                e1.printStackTrace();
+            }
+            return null;
+        }
+    }
+
+    private ByteBuffer mPacketBuffer = ByteBuffer.allocate(MAX_PACKET_LENGTH);
+    DatagramChannel mChannel;
+
+    private void sendMovePacket(float x, float y) {
+        if (mServerAddr != null) {
+            mPacketBuffer.clear();
+            mPacketBuffer.put(PACKET_TYPE_MOVE);
+            mPacketBuffer.putLong(System.currentTimeMillis());
+            mPacketBuffer.putFloat(x);
+            mPacketBuffer.putFloat(y);
+            mPacketBuffer.flip();
+
+            try {
+                mChannel.send(mPacketBuffer, mServerAddr);
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void onNewPosition(float x, float y) {
+        // update the UI
+        mVwCoordinate.setPosition((float) x, (float) y);
+        mTvAcceleration.setText(String.format("%.4f %.4f", x, y));
+        mVwCoordinate.invalidate();
+
+        sendMovePacket(x, y);
+    }
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
+        mBtnRetry = (Button) findViewById(R.id.btn_retry);
+        mVwCoordinate = (CoordinateView) findViewById(R.id.vw_coordinate);
+        mTvAcceleration = (TextView) findViewById(R.id.tv_acc);
+        mTbSwitch = (ToggleButton) findViewById(R.id.tb_switch);
+        mLayoutControl = (ViewGroup) findViewById(R.id.layout_control);
+        mLayoutNoServer = (ViewGroup) findViewById(R.id.layout_no_server);
+        mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+
+        mTbSwitch.setChecked(false);
+        mLayoutControl.setVisibility(View.GONE);
+        mBtnRetry.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new DiscoverTask().execute();
+            }
+        });
+
+        try {
+            mChannel = DatagramChannel.open();
+            mChannel.configureBlocking(false);
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        try {
+            mChannel.close();
+        } catch (IOException e) {
+        }
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mSensorManager.registerListener(this, mSensorManager.getDefaultSensor(Sensor.TYPE_GRAVITY),
+                SensorManager.SENSOR_DELAY_GAME);
+        new DiscoverTask().execute();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mTbSwitch.setChecked(false);
+        mSensorManager.unregisterListener(this);
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        boolean valuesUpdated = false;
+        float values[] = event.values;
+
+        // 转动与移动镜面对称？
+        float newX = -values[0] / 9.81f;
+
+        // 世界坐标系Y轴正向与屏幕坐标系相反
+        float newY = -values[1] / 9.81f;
+
+        if (mTbSwitch.isChecked()) {
+            // 过滤传感器的误差
+            float deltaX = 0f;
+            float deltaY = 0f;
+            if (Math.abs(newX - mLastX) > EPSILON) {
+                deltaX = newX - mLastX;
+                mLastX = newX;
+                valuesUpdated = true;
+            }
+            if (Math.abs(newY - mLastY) > EPSILON) {
+                deltaY = newY - mLastY;
+                mLastY = newY;
+                valuesUpdated = true;
+            }
+            if (valuesUpdated) {
+                onNewPosition(deltaX, deltaY);
+            }
+        } else {
+            mLastX = newX;
+            mLastY = newY;
+        }
+    }
+}
---
+++ PhoneMouseServer/src/com/raphaelyu/phonemouse/PhoneMouseServer.java
@@ -0,0 +1,159 @@
+package com.raphaelyu.phonemouse;
+
+import java.awt.AWTException;
+import java.awt.Dimension;
+import java.awt.MouseInfo;
+import java.awt.Point;
+import java.awt.PointerInfo;
+import java.awt.Robot;
+import java.awt.Toolkit;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.InetSocketAddress;
+import java.net.SocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.DatagramChannel;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+
+public class PhoneMouseServer {
+
+    final static boolean DEBUG_MODE = false;
+
+    final static short PHONE_MOUSE_PORT = 5329;
+    final static int MAX_PACKET_LENGTH = 32;
+    final static byte PACKET_TYPE_DISCOVER = 0x1;
+    final static byte PACKET_TYPE_REPLY = 0x2;
+    final static byte PACKET_TYPE_MOVE = 0x10;
+
+    private static final PrintWriter STD_OUT = new PrintWriter(System.out, true);
+
+    private BlockingQueue<Motion> moves = new ArrayBlockingQueue<Motion>(2000);
+    private Robot mRobot;
+    private int mScreenWidth;
+    private int mScreenHeight;
+    private int mMaxMotionDist;
+
+    private class Motion {
+
+        public Motion(long timestamp, float x, float y) {
+            this.timestamp = timestamp;
+            this.x = x;
+            this.y = y;
+        }
+
+        public long timestamp;
+        public float x;
+        public float y;
+    }
+
+    private class SocketThread extends Thread {
+        private DatagramChannel mServerChannel;
+        private ByteBuffer buffer;
+
+        public SocketThread() throws IOException {
+            super("Receiver");
+            mServerChannel = DatagramChannel.open();
+            mServerChannel.socket().bind(new InetSocketAddress(PHONE_MOUSE_PORT));
+            buffer = ByteBuffer.allocate(MAX_PACKET_LENGTH);
+        }
+
+        /**
+         * Receive the Discover packets and reply.
+         */
+        @Override
+        public void run() {
+            while (true) {
+                buffer.clear();
+                SocketAddress addr;
+                try {
+                    addr = mServerChannel.receive(buffer);
+                    buffer.flip();
+                    if (buffer.limit() > 0) {
+                        byte type = buffer.get();
+                        printLog("received from: " + addr.toString());
+                        switch (type) {
+                        case PACKET_TYPE_DISCOVER:
+                            printlnLog(", type: DISCOVER");
+                            buffer.clear();
+                            buffer.put(PACKET_TYPE_REPLY);
+                            buffer.flip();
+                            try {
+                                mServerChannel.send(buffer, addr);
+                            } catch (IOException e) {
+                                // TODO Auto-generated catch block
+                                e.printStackTrace();
+                            }
+                            break;
+                        case PACKET_TYPE_MOVE:
+                            printlnLog(", type: MOVE");
+                            long timestamp = buffer.getLong();
+                            float moveX = buffer.getFloat();
+                            float moveY = buffer.getFloat();
+                            Motion motion = new Motion(timestamp, moveX, moveY);
+                            moves.offer(motion);
+                            break;
+                        default:
+                            // otherwise ignore the packet.
+                            printlnLog(", type: UNKNOWN, " + type);
+                        }
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }   // end of while(true)
+        }
+    }
+
+    private void init() throws AWTException {
+        mRobot = new Robot();
+        Dimension scrSize = Toolkit.getDefaultToolkit().getScreenSize();
+        mScreenHeight = scrSize.height;
+        mScreenWidth = scrSize.width;
+
+        // 取屏幕长宽的最小值
+        mMaxMotionDist = (mScreenHeight < mScreenWidth) ? mScreenHeight : mScreenWidth;
+    }
+
+    public void start() throws InterruptedException, AWTException, IOException {
+        init();
+        SocketThread th = new SocketThread();
+        th.start();
+        while (true) {
+            long lastTime = 0l;
+            Motion motion = moves.take();
+            if (motion.timestamp > lastTime) {
+                PointerInfo info = MouseInfo.getPointerInfo();
+                if (info != null) {
+                    Point point = info.getLocation();
+                    mRobot.mouseMove((int) (motion.x * mMaxMotionDist) + point.x,
+                            (int) (motion.y * mMaxMotionDist) + point.y);
+                }
+                lastTime = motion.timestamp;
+            }
+        }
+    }
+
+    private void printLog(String str) {
+        if (DEBUG_MODE) {
+            STD_OUT.print(str);
+        }
+    }
+    
+    private void printlnLog(String str){
+        if (DEBUG_MODE) {
+            STD_OUT.println(str);
+        }
+    }
+
+    /**
+     * @param args
+     * @throws InterruptedException
+     * @throws AWTException
+     * @throws IOException
+     */
+    public static void main(String[] args) throws InterruptedException, AWTException, IOException {
+        PhoneMouseServer server = new PhoneMouseServer();
+        server.start();
+    }
+}
